cbc5593ee8435783801adf637fd243f149875155
AtabekDjaparov <aaab02933@gmail.com>
2026-04-14 23:08:21 +0600

    Добавлена функция "main" для обращения к пользователю

--- atabek.py
+++ atabek.py
@@ -1,3 +1,7 @@
 def reverse_words(sentence: str) -> str:
     words = sentence.strip().split()
     return ' '.join(reversed(words))
+    
+def main():
+    print("=== Программа: Обратный порядок слов ===")
+    print("Введите 'выход' (или 'exit') для завершения работы.\n")
